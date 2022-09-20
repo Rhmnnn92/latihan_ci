3708cebfda16918c01b497f5b77b12fa5c670f18
Rhmnnn92 <rohmannnnn14@gmail.com>
2022-09-20 15:11:06 +0700

    Tugas codeigniter

---
+++ application/views/tampil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Pertama</title>
+</head>
+<body>
+    <P>Latihan pertama belajar codeigniter</P>
+    <p>Framework Codeigniter sangat mudah dipelajari!</p>
+</body>
+</html>
