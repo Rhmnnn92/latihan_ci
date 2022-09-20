82c74a6dbb7a34b4eecb5da1388862d7ac237c95
Rhmnnn92 <rohmannnnn14@gmail.com>
2022-09-20 15:48:51 +0700

    Tugas codeigniter

--- application/views/tampil.php
+++ application/views/tampil.php
@@ -5,11 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Pertama</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-    <P class="text-success">Latihan pertama belajar codeigniter</P>
-    <p class="text-success">Framework Codeigniter sangat mudah dipelajari!</p>
+    <P>Latihan pertama belajar codeigniter</P>
+    <p>Framework Codeigniter sangat mudah dipelajari!</p>
 </body>
 </html>
